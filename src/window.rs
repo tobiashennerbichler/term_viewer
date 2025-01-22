927c10e0c293b37f9081e890dac915227cd5ed6a
Tobias Hennerbichler <hennerbichler.tobias@gmx.at>
2025-01-22 21:01:21 +0100

    structure Window into different substructures, read and store entire directory at once, only update dir when button is pressed, break selection right now

--- src/window.rs
+++ src/window.rs
@@ -4,6 +4,7 @@ use std::io::{Error, Read, Write};
 use std::path::{Path, PathBuf};
 use std::fs::read_dir;
 use std::env::current_dir;
+use std::thread::current;
 
 use termios::{tcgetattr, tcsetattr, Termios, ICANON, ECHO, VMIN, TCSADRAIN};
 use termsize::Size;
@@ -17,32 +18,39 @@ struct Page {
 struct FileInfo {
     path: PathBuf,
     file_name: String,
-    canon_name: String,
-    selected: bool,
-    redraw: bool
+    canon_name: String
 }
 
-impl PartialEq for FileInfo {
-    fn eq(&self, other: &Self) -> bool {
-        self.file_name != other.file_name ||
-        self.canon_name != other.canon_name ||
-        self.selected != other.selected
-    }
+//impl PartialEq for FileInfo {
+//    fn eq(&self, other: &Self) -> bool {
+//        self.file_name != other.file_name ||
+//        self.canon_name != other.canon_name ||
+//        self.selected != other.selected
+//    }
+//
+//    fn ne(&self, other: &Self) -> bool {
+//        !self.eq(other)
+//    }
+//}
 
-    fn ne(&self, other: &Self) -> bool {
-        !self.eq(other)
-    }
-}
-
-pub struct Window {
+struct WindowMetadata {
     term_size: Size,
     header_reserved: usize,
     footer_reserved: usize,
-    num_printable_lines: usize,
+    num_printable_lines: usize
+}
+
+struct DirState {
+    path: PathBuf,
+    name: String,
+    files: Vec<FileInfo>
+}
+
+pub struct Window {
+    metadata: WindowMetadata,
+    dir_state: DirState,
     pos: CursorPos,
     page: Page,
-    dir_name: String,
-    current_dir_state: Vec<FileInfo>,
     prev_termios: Termios
 }
 
@@ -66,22 +74,24 @@ impl Window {
         if term_height <= total_reserved + 1 {
             return Err(Error::other("Terminal not big enough to fit one file"));
         }
-
         let num_printable_lines = term_height - total_reserved;
+        let metadata = WindowMetadata {term_size, header_reserved, footer_reserved, num_printable_lines};
+
+        let path = current_dir()?;
+        let Some(name) = path_to_string(&path) else {
+            return Err(Error::other("Cannot get directory name"));
+        };
+        let files = Vec::with_capacity(num_printable_lines);
+        let dir_state = DirState {path, name, files};
+
         let pos = CursorPos {x: 1, y: 3};
         let page = Page {x_page: 0, y_page: 0};
-        let dir_name = String::new();
-        let current_dir_state = Vec::with_capacity(num_printable_lines);
         let prev_termios = get_termios()?;
         Ok(Window {
-            term_size,
-            header_reserved,
-            footer_reserved,
-            num_printable_lines,
+            metadata,
+            dir_state,
             pos,
             page,
-            dir_name,
-            current_dir_state,
             prev_termios
         })
     }
@@ -90,20 +100,19 @@ impl Window {
     pub fn do_interactive(&mut self) -> std::io::Result<()> {
         let mut writer = std::io::stdout();
         ansi::erase(Erase::SCREEN, &mut writer)?;
+        self.update(&mut writer)?;
 
         loop {
-            self.update(&mut writer)?;
-
-            let mut buf = [0; 1];
-            if let Err(_) = std::io::stdin().read_exact(&mut buf) {
-               continue; 
-            }
-            match buf[0] {
-                b'w' => self.pos.y -= 1,
-                b's' => self.pos.y += 1,
-                b'\n' => println!("enter"),
-                b'q' => break,
-                _ => continue
+            let Ok(input) = read_input() else {
+                continue;
+            };
+            match input {
+                b'w'  => self.move_up(&mut writer)?,
+                b's'  => self.move_down(&mut writer)?,
+                b'\n' => self.enter_dir()?,
+                b'u'  => self.update(&mut writer)?,
+                b'q'  => break,
+                _     => continue
             }
             
             std::thread::sleep(std::time::Duration::from_millis(10));
@@ -115,113 +124,90 @@ impl Window {
     fn read_current_dir(&mut self) -> std::io::Result<()> {
         self.update_term_size()?;
 
-        let current_dir = current_dir()?;
-        let entries = read_dir(&current_dir)?;
-        let entry_offset = self.page.x_page * self.num_printable_lines;
-        let mut dir_state = Vec::with_capacity(self.num_printable_lines);
+        let mut entries = read_dir(&self.dir_state.path)?;
+        let mut files= Vec::with_capacity(self.metadata.num_printable_lines);
 
-        let Some(dir_name) = path_to_str(&current_dir) else {
-            return Err(Error::other("Could not convert current dirname to str"));
-        };
-        let dir_name = String::from(dir_name);
-        
         // First entry on first page is parent if it exists
-        let mut line_index = 0;
-        if self.page.x_page == 0 {
-            if let Some(parent_dir) = current_dir.parent() {
+        if self.page.y_page == 0 {
+            if let Some(parent_dir) = self.dir_state.path.parent() {
                 let parent_info = FileInfo {
                     path: parent_dir.to_path_buf(),
                     file_name: String::from(".."),
-                    canon_name: String::from(".."),
-                    selected: line_index == self.pos_to_line_index(self.pos.y),
-                    redraw: true
+                    canon_name: String::from("..")
                 };
-                line_index += 1;
-                dir_state.push(parent_info);
+                files.push(parent_info);
             }
         }
 
         // Read max num_printable_lines directory entries
-        for entry in entries.skip(entry_offset) {
-            if line_index >= self.num_printable_lines {
-                break;
-            }
-
+        for entry in entries {
             let dir_entry = entry?;
             let path = dir_entry.path();
-            let Some(filename) = path_to_str(&path) else {
+            let Some(file_name) = path_to_string(&path) else {
                 continue;
             };
-            let file_name = String::from(filename);
 
             let canon = path.canonicalize()?;
-            let Some(canon_name) = path_to_str(&canon) else {
+            let Some(canon_name) = path_to_string(&canon) else {
                 continue;
             };
-            let canon_name = String::from(canon_name);
 
             let file_info = FileInfo {
                 path,
                 file_name,
-                canon_name,
-                selected: line_index == self.pos_to_line_index(self.pos.y),
-                redraw: true
+                canon_name
             };
-            line_index += 1;
-            dir_state.push(file_info);
+            files.push(file_info);
         }
-
-        self.set_redraws(&mut dir_state);
-        self.dir_name = dir_name;
-        self.current_dir_state = dir_state;
+        
+        //self.set_redraws(&mut files);
+        self.dir_state.files = files;
         Ok(())
     }
 
     fn print_current_dir<W: Write>(&mut self, writer: &mut W) -> std::io::Result<()> {
-        self.print_header(writer, &self.dir_name)?;
+        self.print_header(writer, &self.dir_state.name)?;
  
         let mut line_index = 0;
-        for info in self.current_dir_state.iter() {
-            if !info.redraw {
-                ansi::next_line(writer)?;
-                continue;
-            }
-            ansi::erase(Erase::LINE, writer)?;
-
-            let index = if info.path.is_file() { 0 } else { 1 };
-            print!("{} ", SYMBOLS[index]);
-            let mut text = String::from(&info.file_name);
-            if info.path.is_symlink() {
-                text.push_str(&format!(" {} {}", SYMBOLS[3], &info.canon_name));
-            }
-            self.print_highlighted_if(line_index, &text, writer)?;
-            ansi::next_line(writer)?;
+        let entry_offset = self.page.y_page * self.metadata.num_printable_lines;
+        for info in self.dir_state.files.iter().skip(entry_offset) {
+            self.print_line(writer, info, line_index)?;
 
             line_index += 1;
         }
         ansi::erase(Erase::CURSOR_TO_END, writer)?;
         self.print_footer(writer)?;
-        self.select_current_line(writer)?;
+        //self.select_current_line(writer)?;
         writer.flush()?;
 
         Ok(())
     }
+    
+    fn print_line<W: Write>(&self, writer: &mut W, info: &FileInfo, line_index: usize) -> std::io::Result<()> {
+        ansi::erase(Erase::LINE, writer)?;
 
-    fn update<W: Writer>(&mut self, writer: &mut W) -> std::io::Result<()> {
-        self.read_current_dir()?;
-        self.print_current_dir(writer)
+        let index = if info.path.is_file() { 0 } else { 1 };
+        print!("{} ", SYMBOLS[index]);
+        let mut text = String::from(&info.file_name);
+        if info.path.is_symlink() {
+            text.push_str(&format!(" {} {}", SYMBOLS[3], &info.canon_name));
+        }
+        self.print_highlighted_if(line_index, &text, writer)?;
+        ansi::next_line(writer)?;
+
+        Ok(())
     }
 
-    fn print_header<W: Write>(&self, writer: &mut W, dir_name: &str) -> std::io::Result<()> {
+    fn print_header<W: Write>(&self, writer: &mut W, name: &str) -> std::io::Result<()> {
         ansi::reset_cursor(writer)?;
 
         // Print directory name
-        let dir_name= format!("{} {}", SYMBOLS[2], dir_name);
-        ansi::set_foreground_color(writer, &dir_name, &HEADER_COLOR)?;
+        let name= format!("{} {}", SYMBOLS[2], name);
+        ansi::set_foreground_color(writer, &name, &HEADER_COLOR)?;
         ansi::next_line(writer)?;
 
         // Print divider
-        let len = dir_name.chars().count() + 2;
+        let len = name.chars().count() + 2;
         let divider = String::from_iter(std::iter::repeat_n("-", len));
         ansi::set_foreground_color(writer, &divider, &HEADER_COLOR)?;
         ansi::next_line(writer)?;
@@ -230,7 +216,7 @@ impl Window {
     }
 
     fn print_footer<W: Write>(&self, writer: &mut W) -> std::io::Result<()> {
-        let footer_start = self.header_reserved + self.num_printable_lines + 1;
+        let footer_start = self.metadata.header_reserved + self.metadata.num_printable_lines + 1;
         ansi::set_cursor(CursorPos {x: 1, y: footer_start}, writer)?;
         
         let page_text = format!("Page: {}", self.page.y_page);
@@ -260,24 +246,43 @@ impl Window {
         Ok(())
     }
 
-    fn select_current_line<W: Write>(&mut self, writer: &mut W) -> std::io::Result<()> {
-        let line_index = self.pos_to_line_index(self.pos.y);
-        if line_index >= self.current_dir_state.len() {
-            panic!("Cursor points to non-existing entry");
-        }
-
-        let selected_entry = &self.current_dir_state[line_index];
-        let symbol_overhead = 3;
-        let x = if selected_entry.path.is_symlink() {
-            selected_entry.file_name.chars().count() + selected_entry.canon_name.chars().count() + 2*symbol_overhead + 1
-        } else {
-            selected_entry.file_name.chars().count() + symbol_overhead + 1
-        };
-        
-        self.pos.x = x;
-        ansi::set_cursor(self.pos, writer)?;
+    fn move_up<W: Write>(&mut self, writer: &mut W) -> std::io::Result<()> {
+        self.pos.y -= 1;
         Ok(())
     }
+    
+    fn move_down<W: Write>(&mut self, writer: &mut W) -> std::io::Result<()> {
+        self.pos.y += 1;
+        Ok(())
+    }
+
+    fn enter_dir(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+
+    fn update<W: Write>(&mut self, writer: &mut W) -> std::io::Result<()> {
+        self.read_current_dir()?;
+        self.print_current_dir(writer)
+    }
+
+    //fn select_current_line<W: Write>(&mut self, writer: &mut W) -> std::io::Result<()> {
+    //    let line_index = self.pos_to_line_index(self.pos.y);
+    //    if line_index >= self.dir_state.shown_entries.len() {
+    //        panic!("Cursor points to non-existing entry");
+    //    }
+
+    //    let selected_entry = &self.dir_state.shown_entries[line_index];
+    //    let symbol_overhead = 3;
+    //    let x = if selected_entry.path.is_symlink() {
+    //        selected_entry.file_name.chars().count() + selected_entry.canon_name.chars().count() + 2*symbol_overhead + 1
+    //    } else {
+    //        selected_entry.file_name.chars().count() + symbol_overhead + 1
+    //    };
+    //    
+    //    self.pos.x = x;
+    //    ansi::set_cursor(self.pos, writer)?;
+    //    Ok(())
+    //}
 
     pub fn setup_termios(&self) -> std::io::Result<()> {
         let fd = std::io::stdin().as_raw_fd();
@@ -301,40 +306,52 @@ impl Window {
         }
     }
 
-    fn set_redraws(&mut self, dir_state: &mut Vec<FileInfo>) {
-        for (line_index, (info_old, info_new)) in self.current_dir_state.iter().zip(dir_state).enumerate() {
-            if info_old == info_new && line_index != self.pos_to_line_index(self.pos.y) {
-                info_new.redraw = false;
-            }
-        }
-    }
+    //fn set_redraws(&mut self, shown_entries: &mut Vec<FileInfo>) {
+    //    // Don't redraw line unless changed or if needs highlighting
+    //    for (line_index, (info_old, info_new)) in self.dir_state.shown_entries.iter().zip(shown_entries).enumerate() {
+    //        if info_old == info_new && line_index != self.pos_to_line_index(self.pos.y) {
+    //            info_new.redraw = false;
+    //        }
+    //    }
+    //}
 
     fn update_term_size(&mut self) -> std::io::Result<()> {
         let term_size = termsize::get().expect("Could not get terminal size");
+        if term_size.cols == self.metadata.term_size.cols &&
+           term_size.rows == self.metadata.term_size.rows {
+            return Ok(());
+        }
+
         let term_height = term_size.rows as usize;
-        let total_reserved = self.header_reserved + self.footer_reserved;
+        let total_reserved = self.metadata.header_reserved + self.metadata.footer_reserved;
         if term_height <= total_reserved + 1 {
             return Err(Error::other("Terminal not big enough to fit one file"));
         }
 
-        self.term_size = term_size;
-        self.num_printable_lines = term_height - total_reserved;
-        let last_line = self.header_reserved + self.num_printable_lines;
+        self.metadata.term_size = term_size;
+        self.metadata.num_printable_lines = term_height - total_reserved;
+        let last_line = self.metadata.header_reserved + self.metadata.num_printable_lines;
         self.pos.y = std::cmp::min(self.pos.y, last_line);
         Ok(())
     }
 
     fn pos_to_line_index(&self, y: usize) -> usize {
-        if y <= self.header_reserved || y > self.header_reserved + self.num_printable_lines {
+        if y <= self.metadata.header_reserved || y > self.metadata.header_reserved + self.metadata.num_printable_lines {
             panic!("CursorPosition Y out of bound");
         }
         
-        y - self.header_reserved - 1
-    }
+        y - self.metadata.header_reserved - 1
+    }  
 }
 
-fn path_to_str(path: &Path) -> Option<&str> {
-    path.file_name().and_then(|os_filename| os_filename.to_str())
+fn read_input() -> std::io::Result<u8> {
+    let mut buf = [0; 1];
+    std::io::stdin().read_exact(&mut buf)?;
+    Ok(buf[0])
+}
+
+fn path_to_string(path: &Path) -> Option<String> {
+    path.file_name().and_then(|os_filename| os_filename.to_str().and_then(|file_name| Some(String::from(file_name))))
 }
 
 fn get_termios() -> std::io::Result<Termios> {
